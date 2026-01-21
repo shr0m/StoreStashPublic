771c22e70e88524ea5a78bd4abeaa2d5a7bfe930
shr0m <shr0m@SHR0M-WIN>
2026-01-21 09:41:35 +0000

    Fix network issue

--- src/App.jsx
+++ src/App.jsx
@@ -135,7 +135,7 @@ function EnquireForm() {
     };
 
     try {
-      const res = await fetch("http://localhost:5000/api/enquire", {
+      const res = await fetch("/api/enquire", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
